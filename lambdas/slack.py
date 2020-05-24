c5abf6d5fcd395ca7d4cd6a8a6e9675919ca101a
kmorgan <kiki.morgan13@gmail.com>
2020-05-23 23:13:26 -0400

    update template

--- lambdas/slack.py
+++ lambdas/slack.py
@@ -1,8 +1,32 @@
+import json
+import requests
+import boto3
+
+secret = boto3.client('secretsmanager')
 
 
+def getUrl():
+  urlResponse = secret.get_secret_value(
+    SecretId = '/account-assemble/slack/slackUrl'
+  )
+  url = urlResponse['SecretString']
+  return url
+
+slackUrl = getUrl()
 
 def lambda_handler(event, context):
 
-  message = "slack function works"
+  message = json.dumps(event) 
 
-  return message
+  slackResponse = requests.post(
+    slackUrl,
+    json = {"text": message},
+    headers = {'Content-Type': 'application/json'}
+  )
+
+  http_reply = {
+    "statusCode": slackResponse.status_code,
+    "body": slackResponse.text
+  }
+
+  return http_reply
