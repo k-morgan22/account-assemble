a3b28cc3f7b8b57a9462379d62634d6eb4317318
kmorgan <kiki.morgan13@gmail.com>
2020-05-24 21:45:55 -0400

    update createOu

--- lambdas/createOu.py
+++ lambdas/createOu.py
@@ -1,7 +1,48 @@
+import json
+import boto3
+from uuid import uuid4
 
+ssm = boto3.client('ssm')
+org = boto3.client('organizations')
+
+
+def grabMasterId():
+  
+  listRoots = org.list_roots()
+  
+  return listRoots['Roots'][0]['Id']
+
+
+def createOrgUnit(rootId, ouName):
+
+  ouResponse = org.create_organizational_unit(
+    ParentId = rootId,
+    Name = ouName
+  )
+  
+  return ouResponse['OrganizationalUnit']['Id']
+
+# pre-handler global
+masterId = grabMasterId()
 
 def lambda_handler(event, context):
 
-  message = "createOu function works"
+  masterName = '/account-assemble/orgIds/master-'+ str(uuid4())
+  
+  putParameter = ssm.put_parameter(
+    Name = masterName,
+    Description = 'Master Org Id',
+    Value = masterId,
+    Type = 'String'
+  )
 
-  return message
+  workloadsId = createOrgUnit(masterId, "Workloads")
+  
+  workloadsName = '/account-assemble/orgIds/workloads-'+ str(uuid4())
+  
+  putParameter = ssm.put_parameter(
+    Name = workloadsName,
+    Description = 'Workloads Ou Id',
+    Value = workloadsId,
+    Type = 'String'
+  )
