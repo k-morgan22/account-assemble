0cf4c1e25d679b11b3fdb50e943e01cbcfac9152
kmorgan <kiki.morgan13@gmail.com>
2020-06-02 15:29:28 -0400

    fix custom resource and stackset

--- lambdas/stackset.py
+++ lambdas/stackset.py
@@ -2,11 +2,14 @@ import json
 import boto3
 from uuid import uuid4
 import time
+import logging
 
 ssm = boto3.client('ssm')
 cf = boto3.client('cloudformation')
 ebridge = boto3.client('events')
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def getOuIds(path, decryption):
   response = ssm.get_parameters_by_path(
@@ -24,54 +27,64 @@ def getOuIds(path, decryption):
 workloadsId = getOuIds('/account-assemble/orgIds', False)
 
 def createBaseStack():
+  try:
+    baselineStackName = "account-baseline-" + str(uuid4())  
   
-  baselineStackName = "account-baseline-" + str(uuid4())  
+    baselineStackResponse = cf.create_stack_set(
+      StackSetName = baselineStackName,
+      Description = 'Baseline for new accounts',
+      TemplateURL='https://testing-org-lambda.s3.amazonaws.com/accountAssembleBase.yml',
+      Capabilities= [
+        'CAPABILITY_NAMED_IAM'
+      ],
+      PermissionModel='SERVICE_MANAGED',
+      AutoDeployment={
+        'Enabled': True,
+        'RetainStacksOnAccountRemoval': False
+      }
+    )
+    
+    return baselineStackName
+  except Exception as e:
+    logger.error(e)
+    raise
   
-  baselineStackResponse = cf.create_stack_set(
-    StackSetName = baselineStackName,
-    Description = 'Baseline for new accounts',
-    TemplateURL='https://testing-org-lambda.s3.amazonaws.com/accountAssembleBase.yml',
-    Capabilities= [
-      'CAPABILITY_NAMED_IAM'
-    ],
-    PermissionModel='SERVICE_MANAGED',
-    AutoDeployment={
-      'Enabled': True,
-      'RetainStacksOnAccountRemoval': False
-    }
-  )
   
-  return baselineStackName
 
 
 def deployBaselineStack(stackName, ou):
-  deployBaseResponse = cf.create_stack_instances(
-    StackSetName=stackName,
-    DeploymentTargets={
-      'OrganizationalUnitIds': [
-        ou
-      ]
-    },
-    Regions=[
-      'us-east-1'
-    ],
-    OperationPreferences={
-      'FailureTolerancePercentage': 0,
-      'MaxConcurrentPercentage': 100
-    }
-  )
-  
-  baselineOpId = deployBaseResponse['OperationId']
-  
-  while True:
-    deployBaseStatus = cf.describe_stack_set_operation(
+  try:
+    deployBaseResponse = cf.create_stack_instances(
       StackSetName=stackName,
-      OperationId=baselineOpId
+      DeploymentTargets={
+        'OrganizationalUnitIds': [
+          ou
+        ]
+      },
+      Regions=[
+        'us-east-1'
+      ],
+      OperationPreferences={
+        'FailureTolerancePercentage': 0,
+        'MaxConcurrentPercentage': 100
+      }
     )
-    if deployBaseStatus['StackSetOperation']['Status'] == 'RUNNING':
-      time.sleep(10)
-    elif deployBaseStatus['StackSetOperation']['Status'] == 'SUCCEEDED':
-      break
+    
+    baselineOpId = deployBaseResponse['OperationId']
+    
+    while True:
+      deployBaseStatus = cf.describe_stack_set_operation(
+        StackSetName=stackName,
+        OperationId=baselineOpId
+      )
+      if deployBaseStatus['StackSetOperation']['Status'] == 'RUNNING':
+        time.sleep(10)
+      elif deployBaseStatus['StackSetOperation']['Status'] == 'SUCCEEDED':
+        break
+  except Exception as e:
+    logger.error(e)
+    raise
+  
 
 def putEvent():
   metadata = {
@@ -91,7 +104,10 @@ def putEvent():
   )
 
 def lambda_handler(event, context):
-  envStackName = createBaseStack()
-  deployBaselineStack(envStackName, workloadsId)
+  if "errorCode" not in event:
+    envStackName = createBaseStack()
+    deployBaselineStack(envStackName, workloadsId)
 
-  putEvent()
+    putEvent()
+  else:
+    logger.info("Triggered by error")
